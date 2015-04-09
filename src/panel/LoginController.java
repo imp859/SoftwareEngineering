febd2ed292db3521b7182da41b3c1895c9f9812a
Stephen <sleija742@hotmail.com>
2015-04-08 20:08:44 -0500

    added call to session in controller, setters, and getters of session state in model

--- src/panel/LoginController.java
+++ src/panel/LoginController.java
@@ -16,26 +16,24 @@ public class LoginController implements ActionListener {
 	// temporary boolean method. will probably return a session state of a user
 	// later.
 	// these parameters passed from view
-	public boolean checkCredentials(String userName, char[] passwordArray)
+	public Session checkCredentials(String userName, char[] passwordArray)
 			throws Exception {
-		boolean flag = false;
+		//boolean flag = false;
 		String password = new String(passwordArray);
 		// check is kind of a hack. will need to test more
 		for (int i = 0; i < patientModel.listSize(); i++) {
 			if (userName.equals(patientModel.getUserName(i))
 					&& password.equals(patientModel.getUserPassword(i))) {
-				flag = true;
-				break;
+				int userTypeID = patientModel.getUserType();
+				Session session = new Session();
+				session.getSession(userTypeID);
+				return session;
 			}
 		}
 		/*
 		 * TODO throw exception in view. maybe JOptionPane?
 		 */
-		if (flag == false){
-			throw new Exception();
-		}		
-
-		return flag;
+		throw new Exception();
 	}
 
 	public void actionPerformed(ActionEvent e) {
